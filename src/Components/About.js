629772fa61aa883949664ef42b1c984438001902
Nick Tillinghast <nickt2@cox.net>
2020-03-24 14:05:53 -0700

    deleted media for sizing

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,21 +1,21 @@
-import React from "react";
-import "./About.css";
+// import React from "react";
+// import "./About.css";
 
-export default function About() {
-  return (
-    <div className="about-main">
-      <div className="about-body">
-        <h1 className="about-blurb">
-          Junior's Barbers provide high quality traditional, short hair cuts, as
-          well as contemporary and trendy styles and much more.
-        </h1>
-        <h2 className="about-blurb">
-          We pride ourselves on service in a comfortable and clean environment
-        </h2>
-        <div className="about-iframe">
+// export default function About() {
+//   return (
+//     <div className="about-main">
+//       <div className="about-body">
+//         <h1 className="about-blurb">
+//           Junior's Barbers provide high quality traditional, short hair cuts, as
+//           well as contemporary and trendy styles and much more.
+//         </h1>
+//         <h2 className="about-blurb">
+//           We pride ourselves on service in a comfortable and clean environment
+//         </h2>
+//         <div className="about-iframe">
 
-        </div>
-      </div>
-    </div>
-  );
-}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
