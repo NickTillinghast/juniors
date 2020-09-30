b2b146b7c35e9181aa7b6e91f8122653148f7b43
Nick Tillinghast <nickt2@cox.net>
2020-09-29 23:16:09 -0600

    fix

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
-const path = require("path");
-app.use(express.static(path.join(__dirname, "/../build")));
+// const path = require("path");
+// app.use(express.static(path.join(__dirname, "/../build")));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../build/index.html"));
-});
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../build/index.html"));
+// });
+const { SERVER_PORT } = process.env;
+const port = SERVER_PORT;
+
+app.listen(port, () => console.log(`listening on port ${port}`));
