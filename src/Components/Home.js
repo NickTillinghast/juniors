327b2ba4fbf7e0a9b59d127a70a8fa0c5bfd5ce5
Nick Tillinghast <nickt2@cox.net>
2020-10-09 21:58:12 -0600

    please

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -12,6 +12,7 @@ export default function Home() {
             <div className="home-main-text">
               <h1 className="home-title">Junior's Barber Parlor</h1>
               <h3 className="home-title-2">since 2016</h3>
+              <p className="home-middle-text-h1">Due to Covid 19 Our Waiting area is closed and Masks are required. </p>
             </div>
           </div>
         </div>
